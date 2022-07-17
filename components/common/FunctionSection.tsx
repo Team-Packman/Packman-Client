cdceba965d176a84db2946f089583edec42bcc2b
n0eyes <doll4586633@naver.com>
2022-07-18 08:13:10 +0900

    css/#22 : Add z-index

--- components/common/FunctionSection.tsx
+++ components/common/FunctionSection.tsx
@@ -24,7 +24,7 @@ const StyledRoot = styled.section`
   box-shadow: 0px -3px 13px rgba(0, 0, 0, 0.05);
   padding: 1.6rem 2rem;
   bottom: 0;
-  z-index: 99;
+  z-index: 40;
   & > button:first-child {
     margin-bottom: 0.9rem;
   }
