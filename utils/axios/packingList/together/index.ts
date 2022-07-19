09204b2ec4e0793b641de2139e3d8394819083ba
n0eyes <doll4586633@naver.com>
2022-07-20 06:33:36 +0900

    feat/#48 : 함께 패킹 리스트 서버 연결 성공(예시 코드)

--- utils/axios/packingList/together/index.ts
+++ utils/axios/packingList/together/index.ts
@@ -1,19 +1,72 @@
-import { GetTogetherPackingListDeatilOutput } from './../../../../service/packingList/together/index';
-import { GroupMembersOutput } from '../../../../service/packingList/together/index';
+import {
+  GetTogetherPackingListDeatilOutput,
+  AddTogetherPackingListCategoryInput,
+  AddTogetherPackingListCategoryOutput,
+  UpdateTogetherPackingListCategoryInput,
+  UpdateTogetherPackingListCategoryOutput,
+  AddTogetherPackingListItemInput,
+  AddTogetherPackingListItemOutput,
+  UpdateTogetherPackingListItemInput,
+  UpdateTogetherPackingListItemOutput,
+  UpdatePackingListTitleInput,
+  UpdatePackingListTitleOutput,
+} from './../../../../service/packingList/together/index';
+import { GetGroupMembersOutput } from '../../../../service/packingList/together/index';
 import { AxiosInstance } from 'axios';
+
+export const fetchUpdatePackingListTitle = async (
+  request: AxiosInstance,
+  payload: UpdatePackingListTitleInput,
+): Promise<UpdatePackingListTitleOutput> => {
+  const { data } = await request.patch(`/packingList/title`, payload);
+  return data;
+};
 
 export const fetchGroupMember = async (
   request: AxiosInstance,
   groupId: string,
-): Promise<GroupMembersOutput> => {
+): Promise<GetGroupMembersOutput> => {
   const { data } = await request(`/together/member/${groupId}`);
   return data;
 };
 
 export const fetchPackingListDetail = async (
   request: AxiosInstance,
-  pacingListId: string,
+  listId: string,
+  inviteCode: string = '',
 ): Promise<GetTogetherPackingListDeatilOutput> => {
-  const { data } = await request(`/together/${pacingListId}`);
+  const { data } = await request(`/packingList/together/${listId}?invitecCode=${inviteCode}`);
+  return data;
+};
+
+export const fetchAddPackingListCategory = async (
+  request: AxiosInstance,
+  payload: AddTogetherPackingListCategoryInput,
+): Promise<AddTogetherPackingListCategoryOutput> => {
+  const { data } = await request.post(`/packingList/together/category`, payload);
+  return data;
+};
+
+export const fetchUpdatePackingListCategory = async (
+  request: AxiosInstance,
+  payload: UpdateTogetherPackingListCategoryInput,
+): Promise<UpdateTogetherPackingListCategoryOutput> => {
+  const { data } = await request.patch(`/packingList/together/category`, payload);
+  return data;
+};
+
+export const fetchAddPackingListItem = async (
+  request: AxiosInstance,
+  payload: AddTogetherPackingListItemInput,
+): Promise<AddTogetherPackingListItemOutput> => {
+  const { data } = await request.post(`/packingList/together/pack`, payload);
+  return data;
+};
+
+export const fetchUpdatePackingListItem = async (
+  request: AxiosInstance,
+  payload: UpdateTogetherPackingListItemInput,
+): Promise<UpdateTogetherPackingListItemOutput> => {
+  const { data } = await request.patch(`/packingList/together/pack`, payload);
   return data;
 };
