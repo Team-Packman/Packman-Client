d6d3cb99b945b7696c1177b47a31e67959220aa1
yunsun <yunsun99@google.com>
2022-07-22 00:45:33 +0900

    feat/#64 : 각종 모달들 스타일링 구현

---
+++ pages/modal-test.tsx
@@ -0,0 +1,8 @@
+import ModalForShare from './components/common/ModalForShare';
+import React from 'react';
+
+function modalTest() {
+  return <ModalForShare></ModalForShare>;
+}
+
+export default modalTest;
