97b5ad95b92c40220ef67131bc9402bc8d63acd4
leeseooo <leeseoooo@naver.com>
2022-08-10 16:01:56 +0900

    feat/#147 : vconsole 테스트

--- utils/axios/withAuth.tsx
+++ utils/axios/withAuth.tsx
@@ -8,7 +8,10 @@ function withAuth(axios: AxiosInstance, user: User) {
   const requestIntercept = axios.interceptors.request.use(
     (config: AxiosRequestConfig) => {
       if (config.headers && !config.headers['Authorization']) {
-        config.headers['Authorization'] = `${user.accessToken}`;
+        // config.headers['Authorization'] = `${user.accessToken}`;
+        config.headers[
+          'Authorization'
+        ] = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJmMzRlN2U1ZDI1Mzc4NTQ4MmRjYTQ1In0sImlhdCI6MTY2MDExMjgwNiwiZXhwIjoxNjYxODQwODA2fQ.hv1BG1BNbXM9dlxN_hD__s3YIuj1mxd4ET5wKq-ptSE`;
         return config;
       }
 
