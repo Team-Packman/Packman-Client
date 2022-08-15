1323213af9d6a4debf0decb3c9562a0144fadf51
n0eyes <doll4586633@naver.com>
2022-08-15 16:24:13 +0900

    feat/#160 : 패킹 아이템 width 조정

--- components/common/PackagesWithCategory.tsx
+++ components/common/PackagesWithCategory.tsx
@@ -33,7 +33,6 @@ function PackagesWithCategory(props: PackagesWithCategoryProps) {
 export default PackagesWithCategory;
 
 const StyledRoot = styled.ul`
-  width: 100%;
   & > ul {
     margin-top: 0.8rem;
   }
