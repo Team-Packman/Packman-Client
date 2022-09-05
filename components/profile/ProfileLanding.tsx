c100c423b783293b5e396f522bed3e36d70ed39e
leeseooo <leeseoooo@naver.com>
2022-09-06 00:13:22 +0900

    chore/#269 : 회원가입 뷰 url로 이동 방지

--- components/profile/ProfileLanding.tsx
+++ components/profile/ProfileLanding.tsx
@@ -1,24 +1,39 @@
+import { useEffect } from 'react';
 import CreateProfile from './CreateProfile';
 import styled from 'styled-components';
 import Layout from '../common/Layout';
+import { useRecoilValue } from 'recoil';
+import { authUserAtom } from '../../utils/recoil/atom/atom';
+import { useRouter } from 'next/router';
 
 function ProfileLanding() {
+  const router = useRouter();
+  const { isAlreadyUser } = useRecoilValue(authUserAtom);
+
+  useEffect(() => {
+    if (isAlreadyUser) {
+      router.replace('/folder');
+    }
+  }, []);
+
   return (
-    <Layout padding>
-      <StyledRoot>
-        <CreateProfile
-          comment={
-            <div>
-              <h1>환영합니다 :^)</h1>
-              <h1>
-                <b>팩맨</b>에서 사용하실 <b>프로필</b>을
-              </h1>
-              <h1>완성해주세요.</h1>
-            </div>
-          }
-        />
-      </StyledRoot>
-    </Layout>
+    !isAlreadyUser && (
+      <Layout padding>
+        <StyledRoot>
+          <CreateProfile
+            comment={
+              <div>
+                <h1>환영합니다 :^)</h1>
+                <h1>
+                  <b>팩맨</b>에서 사용하실 <b>프로필</b>을
+                </h1>
+                <h1>완성해주세요.</h1>
+              </div>
+            }
+          />
+        </StyledRoot>
+      </Layout>
+    )
   );
 }
 
