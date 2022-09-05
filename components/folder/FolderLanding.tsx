30ff35889c9e81b3634cc5d3b0abed78b4d3f0d2
younyikim <rlakuku1221@gmail.com>
2022-09-05 23:04:16 +0900

    fix/#265: 폴더 디자인 수정 및 최근생성리스트 invalidate 추가

--- components/folder/FolderLanding.tsx
+++ components/folder/FolderLanding.tsx
@@ -51,6 +51,7 @@ function FolderLanding() {
         setIsOutDated(Number(remainDay) < 0);
       }
     },
+    refetchOnMount: true,
   });
 
   const { mutate: editFolderMutate } = useMutation((editedFolderData: ModalDataProps) => {
@@ -119,6 +120,7 @@ function FolderLanding() {
 
     deletFolderMutate(id, {
       onSuccess: () => {
+        queryClient.invalidateQueries('recentPacking');
         queryClient.setQueryData('folderListKey', (oldData: any) => {
           return {
             ...oldData,
@@ -259,7 +261,11 @@ function FolderLanding() {
               </StyledLabel>
               <StyledDday>
                 <StyledRemainDay>
-                  {isOutDated ? 'Done!' : `D-${recentPackingData?.data?.remainDay}`}
+                  {isOutDated
+                    ? 'Done!'
+                    : recentPackingData?.data?.remainDay === '0'
+                    ? 'D-day'
+                    : `D-${recentPackingData?.data?.remainDay}`}
                 </StyledRemainDay>
                 <StyledLeftMessage>
                   {!isOutDated && recentPackingData?.data?.packRemainNum === '0' ? (
@@ -337,6 +343,7 @@ export default FolderLanding;
 
 export const StyledBody = styled.article`
   display: flex;
+  flex: 1;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
