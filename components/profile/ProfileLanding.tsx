b18a152cbf3bb331ce151385ff6bdfc32d569ffb
leeseooo <leeseoooo@naver.com>
2022-09-06 00:15:56 +0900

    chore/#269 : 회원가입 뷰 url로 이동 방지 에러 고침

--- components/profile/ProfileLanding.tsx
+++ components/profile/ProfileLanding.tsx
@@ -17,23 +17,25 @@ function ProfileLanding() {
   }, []);
 
   return (
-    !isAlreadyUser && (
-      <Layout padding>
-        <StyledRoot>
-          <CreateProfile
-            comment={
-              <div>
-                <h1>환영합니다 :^)</h1>
-                <h1>
-                  <b>팩맨</b>에서 사용하실 <b>프로필</b>을
-                </h1>
-                <h1>완성해주세요.</h1>
-              </div>
-            }
-          />
-        </StyledRoot>
-      </Layout>
-    )
+    <>
+      {!isAlreadyUser && (
+        <Layout padding>
+          <StyledRoot>
+            <CreateProfile
+              comment={
+                <div>
+                  <h1>환영합니다 :^)</h1>
+                  <h1>
+                    <b>팩맨</b>에서 사용하실 <b>프로필</b>을
+                  </h1>
+                  <h1>완성해주세요.</h1>
+                </div>
+              }
+            />
+          </StyledRoot>
+        </Layout>
+      )}
+    </>
   );
 }
 
