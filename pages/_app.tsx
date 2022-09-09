e24b16e886929aecfa044186c57b32f935bcf91f
n0eyes <doll4586633@naver.com>
2022-09-09 21:44:29 +0900

    test/main : mobile console.log test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import { RecoilRoot } from 'recoil';
 import InstallGuide from '../components/common/InstallGuide';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log('message test');
   const [show, setShow] = useState(false);
 
   const [queryClient] = useState(
