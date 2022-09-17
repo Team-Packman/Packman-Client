95c1f39e022be8f34bbe1a5bd693beaf9bbab229
leeseooo <leeseoooo@naver.com>
2022-09-17 12:12:32 +0900

    feat/#325 : Component.displayName > 소문자에서 대문자로 변경, meta tag 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import { RecoilRoot } from 'recoil';
 import InstallGuide from '../components/common/InstallGuide';
 import HeadMeta from '../components/HeadMeta';
 import { AsyncBoundary } from '../utils/AsyncBoundary';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [show, setShow] = useState(false);
@@ -36,7 +37,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   if (!show) return null;
-
   return (
     <>
       <HeadMeta page={Component.displayName} />
