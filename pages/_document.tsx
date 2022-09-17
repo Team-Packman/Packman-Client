a625696f12cbdd8a694773f82e9a530f0d0ee201
n0eyes <doll4586633@naver.com>
2022-09-17 15:31:32 +0900

    feat/#327 : Preload font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,10 +42,46 @@ class MyDocument extends Document {
           {/* 검색엔진이 대부분 무시하는 추세 */}
           {/* <meta name="keywords" content="짐,짐 챙기기,여행 짐,여행 체크리스트" /> */}
           {/* 추후 수정 */}
+
           <meta property="og:image" content="/assets/pwa/apple-splash-1136-640.jpg" />
           <meta property="og:url" content="https://www.packman.kr" />
           {/* pwa */}
           <meta name="theme-color" content="#fff" />
+          <link
+            rel="preload"
+            href="/assets/font/Pretendard-Medium.woff2"
+            as="font"
+            crossOrigin=""
+            type="font/woff2"
+          />
+          <link
+            rel="preload"
+            href="/assets/font/Pretendard-SemiBold.woff2"
+            as="font"
+            crossOrigin=""
+            type="font/woff2"
+          />
+          <link
+            rel="preload"
+            href="/assets/font/Pretendard-ExtraBold.woff2"
+            as="font"
+            crossOrigin=""
+            type="font/woff2"
+          />
+          <link
+            rel="preload"
+            href="/assets/font/Pretendard-Light.woff2"
+            as="font"
+            crossOrigin=""
+            type="font/woff2"
+          />
+          <link
+            rel="preload"
+            href="/assets/font/Pretendard-Regular.woff2"
+            as="font"
+            crossOrigin=""
+            type="font/woff2"
+          />
           <link rel="manifest" href="/manifest.json" />
           <meta name="apple-mobile-web-app-title" content="Packman" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
