f3bff50fb28fdcc6612d716aafa078cf3fece589
leeseooo <leeseoooo@naver.com>
2022-09-20 23:00:21 +0900

    feat/#353 : 함께 패킹리스트 초대 모달에서 ssr 하는 방식으로 수정

--- pages/together/index.tsx
+++ pages/together/index.tsx
@@ -1,47 +1,13 @@
-import axios, { AxiosResponse } from 'axios';
-import { GetServerSideProps } from 'next';
 import React from 'react';
-import HeadMeta from '../../components/HeadMeta';
 import TogetherLanding from '../../components/together/TogetherLanding';
-import { GetTogetherPackingListDetailOutput } from '../../service/packingList/together';
 import { AsyncBoundary } from '../../utils/AsyncBoundary';
 
-interface TogetherProps {
-  title: string;
-}
-function Together(props: TogetherProps) {
-  const { title } = props;
-
+function Together() {
   return (
-    <>
-      <HeadMeta title={title} />
-      <AsyncBoundary>
-        <TogetherLanding />
-      </AsyncBoundary>
-    </>
+    <AsyncBoundary>
+      <TogetherLanding />
+    </AsyncBoundary>
   );
 }
 
 export default Together;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const accessToken = context.req.cookies.accessToken as string;
-  const { id } = context.query;
-  const { data }: AxiosResponse<GetTogetherPackingListDetailOutput> = await axios.get(
-    `${process.env.NEXT_PUBLIC_END}/list/together/${id}`,
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    },
-  );
-  const {
-    data: { title },
-  } = data;
-
-  return {
-    props: {
-      title,
-    },
-  };
-};
--- pages/together/invited/index.tsx
+++ pages/together/invited/index.tsx
@@ -1,12 +1,45 @@
+import axios, { AxiosResponse } from 'axios';
+import { GetServerSideProps } from 'next';
+import HeadMeta from '../../../components/HeadMeta';
 import InvitedLanding from '../../../components/together/invited/InvitedLanding';
+import { GetSharedPackingListDetailOutput } from '../../../service/packingList/common';
 import { AsyncBoundary } from '../../../utils/AsyncBoundary';
 
-function Invited() {
+interface InvitedProps {
+  title: string;
+}
+function Invited(props: InvitedProps) {
+  const { title } = props;
   return (
-    <AsyncBoundary>
-      <InvitedLanding />;
-    </AsyncBoundary>
+    <>
+      <HeadMeta title={title} />
+      <AsyncBoundary>
+        <InvitedLanding />;
+      </AsyncBoundary>
+    </>
   );
 }
 
 export default Invited;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const accessToken = context.req.cookies.accessToken as string;
+  const { id } = context.query;
+  const { data }: AxiosResponse<GetSharedPackingListDetailOutput> = await axios.get(
+    `${process.env.NEXT_PUBLIC_END}/list/together/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
+  const {
+    data: { title },
+  } = data;
+
+  return {
+    props: {
+      title,
+    },
+  };
+};
