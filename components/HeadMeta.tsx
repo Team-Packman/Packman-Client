ed6ecade5d918dcf47b03fa11e5206e1e580ebb2
n0eyes <doll4586633@naver.com>
2022-09-20 23:39:43 +0900

    test/main : meta test

--- components/HeadMeta.tsx
+++ components/HeadMeta.tsx
@@ -19,12 +19,7 @@ function HeadMeta(props: HeadMetaProps) {
         name="viewport"
         content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
       />
-      {title ? (
-        <meta property="og:title" content={title} />
-      ) : (
-        <meta property="og:title" content={'팩맨 - 내 손안의 짐 챙김 도우미'} />
-      )}
-
+      <meta property="og:title" content={title || '팩맨 - 내 손안의 짐 챙김 도우미'} />
       <meta
         property="og:description"
         content={description || '내 손안 짐 챙김 도우미, 팩맨. 지금 바로 팩맨을 사용해보세요!'}
