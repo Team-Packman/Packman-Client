a90813882ad8717f667bc056dd126c22b10c678f
leeseooo <leeseoooo@naver.com>
2022-09-24 01:59:57 +0900

    chore/#361 : _app.tsx에 HeadMeta 다시 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,14 +3,16 @@ import HeadMeta from '../components/HeadMeta';
 import LoginLanding from '../components/login/LoginLanding';
 import { AsyncBoundary } from '../utils/AsyncBoundary';
 
-function Login() {
+interface LoginProps {
+  title?: string;
+  description?: string;
+  url?: string;
+}
+function Login(props: LoginProps) {
+  const { title, description, url } = props;
   return (
     <>
-      <HeadMeta
-        title="Packman : 로그인"
-        description="카카오 로그인으로 3초만에 시작하기"
-        url="https://www.packman.kr/login"
-      />
+      <HeadMeta title={title} description={description} url={url} />
       <AsyncBoundary>
         <LoginLanding />
       </AsyncBoundary>
@@ -19,3 +21,11 @@ function Login() {
 }
 
 export default Login;
+
+Login.getInitialProps = () => {
+  return {
+    title: 'Packman : 로그인',
+    description: '카카오 로그인으로 3초만에 시작하기',
+    url: 'https://www.packman.kr/login',
+  };
+};
