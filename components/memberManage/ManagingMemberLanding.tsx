e580cdfb93f97c7b9728a040eb071e35cdd68926
leeseooo <leeseoooo@naver.com>
2022-09-23 20:44:07 +0900

    feat/#356 : 멤버관리 뷰 대장 border 추가 및 프로필 간 간격 수정

--- components/memberManage/ManagingMemberLanding.tsx
+++ components/memberManage/ManagingMemberLanding.tsx
@@ -171,6 +171,7 @@ function ManagingMemberLanding() {
                       height={64}
                       alt="profile_image"
                       priority
+                      layout="responsive"
                     />
                   </MemberImage>
                   <MemberName>{member.nickname}</MemberName>
@@ -186,6 +187,7 @@ function ManagingMemberLanding() {
                     height={64}
                     alt="profile_image"
                     priority
+                    layout="responsive"
                   />
                 </MemberImage>
                 <MemberName>{member.nickname}</MemberName>
@@ -294,8 +296,6 @@ const WithMembers = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(6.4rem, 1fr));
   gap: 2rem;
 
-  height: 30rem;
-
   position: relative;
 `;
 
@@ -323,11 +323,12 @@ const Crown = styled.div`
 `;
 
 const MemberImage = styled.div<{ index: number }>`
+  width: 6.4rem;
+  height: 6.4rem;
   margin-bottom: 0.5rem;
-  & > span {
-    border-radius: 50%;
-    border: ${({ index }) => (index === 0 ? `0.2rem solid ${packmanColors.pmPink}` : 'none')};
-  }
+  border-radius: 50%;
+  border: ${({ index }) => (index === 0 ? `0.2rem solid ${packmanColors.pmPink}` : 'none')};
+  overflow: hidden;
 `;
 
 const MemberName = styled.div`
