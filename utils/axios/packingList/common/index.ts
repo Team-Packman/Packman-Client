42091a5400d4dd43960e37dbf99b8dcc7fd8788f
n0eyes <doll4586633@naver.com>
2022-10-04 23:51:02 +0900

    feat/#368 : 엿보기 로그용 api 추가

--- utils/axios/packingList/common/index.ts
+++ utils/axios/packingList/common/index.ts
@@ -1,4 +1,5 @@
 import {
+  GetHelp,
   GetSharedPackingListDetailInput,
   GetSharedPackingListDetailOutput,
 } from './../../../../service/packingList/common/index';
@@ -9,5 +10,10 @@ export const fetchSharedPackingListDetail = async (
   { type, inviteCode }: GetSharedPackingListDetailInput,
 ): Promise<GetSharedPackingListDetailOutput> => {
   const { data } = await request(`/list/${type}/share/${inviteCode}`);
+  return data;
+};
+
+export const fetchHelp = async (request: AxiosInstance): Promise<GetHelp> => {
+  const { data } = await request(`/help`);
   return data;
 };
