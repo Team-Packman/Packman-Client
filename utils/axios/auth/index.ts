32ef495bfde323c885cc253900f0c84372312b74
leeseooo <leeseoooo@naver.com>
2023-02-23 11:21:15 +0900

    chore/PK-17: 요청 헤더 Bearer 키워드 추가, 카카오 로그인 시크릿 키 쿼리 추가
    feat/PK-17: Dropdown 컴포넌트 생성 및 DropBox 컴포넌트에 적용
    
    feat/PK-17: 멤버 관리 뷰 왕관 쓰는 멤버 코드 수정
    
    feat/PK-17: Dropdown 컴포넌트 수정
    
    feat/PK-17: 스타일 props네이밍 styles로 변경, 드롭다운 이벤트 핸들러 네이밍 onClick > onChange로 변경
    
    chore/PK-17: 스타일 props 네이밍 수정
    
    chore/PK-17: DropBox의 Trigger 컴포넌트에서 회전 스타일 overlay를 받는 방식으로 변경
    
    feat/PK-17: Item 컴포넌트 수정(onChange, 컴포넌트 분리)
    
    chore/PK-17: Item context 제거, FolderDropBox에서 Item에 render props 전달
    
    feat/PK-17: generic, cloneElement, createElement 추가해서 Dropdown 컴포넌트 리팩토링

--- utils/axios/auth/index.ts
+++ utils/axios/auth/index.ts
@@ -16,7 +16,7 @@ export const fetchGoogleLogin = async (payload: GoogleLoginInput): Promise<Googl
 export const fetchKakaoAuth = async (code: string): Promise<KakaoAuthOutput> => {
   const url = encodeURI(process.env.NEXT_PUBLIC_REDIRECT ?? '');
   const { data } = await client.post(
-    `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&code=${code}&redirect_uri=${url}`,
+    `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&code=${code}&redirect_uri=${url}&client_secret=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET}`,
     {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
--- utils/axios/index.tsx
+++ utils/axios/index.tsx
@@ -43,7 +43,7 @@ function AxiosInterceptor({ children }: PropsWithChildren) {
   const requestIntercept = client.interceptors.request.use(
     (config: AxiosRequestConfig) => {
       if (config.headers && !config.headers['Authorization']) {
-        config.headers['Authorization'] = `${accessToken}`;
+        config.headers['Authorization'] = `Bearer ${accessToken}`;
 
         return config;
       }
@@ -66,7 +66,7 @@ function AxiosInterceptor({ children }: PropsWithChildren) {
           const tokens = await refresh();
 
           if (tokens) {
-            config.headers['Authorization'] = `${tokens.accessToken}`;
+            config.headers['Authorization'] = `Bearer ${tokens.accessToken}`;
 
             return client(config);
           }
