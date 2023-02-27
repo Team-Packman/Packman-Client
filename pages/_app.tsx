8c07da84e6531bcb7437a1610e7b1f49a6bcf994
leeseooo <leeseoooo@naver.com>
2023-02-27 23:10:38 +0900

    chore/PK-17: react-query 리렌더링 최적화

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           queries: {
             suspense: true,
             retry: 0,
+            notifyOnChangeProps: 'tracked',
           },
         },
       }),
