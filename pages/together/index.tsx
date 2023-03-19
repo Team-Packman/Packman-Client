cdebd44c29a349a229070af34cc097ed9bc406d7
leeseooo <leeseoooo@naver.com>
2023-03-19 22:08:43 +0900

    hotfix/dev: safari 정규표현식 인식 불가능 이슈로 각 페이지에서 HeadMeta 삭제

--- pages/together/index.tsx
+++ pages/together/index.tsx
@@ -1,47 +1,12 @@
-import { NextPageContext } from 'next';
-import { useRouter } from 'next/router';
-import React from 'react';
-import HeadMeta from '../../components/HeadMeta';
 import TogetherLanding from '../../components/together/TogetherLanding';
-import apiService from '../../service';
 import { AsyncBoundary } from '../../utils/AsyncBoundary';
-import { client } from '../../utils/axios';
 
-interface TogetherProps {
-  title: string;
-}
-
-function Together(props: TogetherProps) {
-  const { title: headerTitle } = props;
-  const router = useRouter();
-
+function Together() {
   return (
-    <>
-      <HeadMeta
-        title={headerTitle}
-        description={`[${headerTitle}] 패킹리스트가 공유되었어요!`}
-        url={`${process.env.NEXT_PUBLIC_DOMAIN}/${router.asPath}`}
-      />
-      <AsyncBoundary>
-        <TogetherLanding />
-      </AsyncBoundary>
-    </>
+    <AsyncBoundary>
+      <TogetherLanding />
+    </AsyncBoundary>
   );
 }
 
 export default Together;
-
-Together.getInitialProps = async function ({ req, query }: NextPageContext) {
-  const getPackingListHeader = apiService.packingList.together.getPackingListHeader;
-  const cookie = req?.headers?.cookie?.split(';');
-  const regex = new RegExp(/(?<=accessToken=).+/, 'g');
-  const accessToken = cookie && cookie[1].match(regex);
-
-  client.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-
-  const { data: header } = await getPackingListHeader(query.id as string, false);
-  const { title } = header;
-  return {
-    title,
-  };
-};
--- pages/together/invited/index.tsx
+++ pages/together/invited/index.tsx
@@ -1,48 +1,12 @@
-import { NextPageContext } from 'next';
-import { useRouter } from 'next/router';
-import HeadMeta from '../../../components/HeadMeta';
 import InvitedLanding from '../../../components/together/invited/InvitedLanding';
-import apiService from '../../../service';
 import { AsyncBoundary } from '../../../utils/AsyncBoundary';
-import { client } from '../../../utils/axios';
 
-interface InvitedProps {
-  title: string;
-}
-function Invited(props: InvitedProps) {
-  const { title } = props;
-  const router = useRouter();
-
+function Invited() {
   return (
-    <>
-      <HeadMeta
-        title={title}
-        description={`[${title}] 패킹리스트가 공유되었어요!`}
-        url={`${process.env.NEXT_PUBLIC_DOMAIN}/${router.asPath}`}
-      />
-      <AsyncBoundary>
-        <InvitedLanding />;
-      </AsyncBoundary>
-    </>
+    <AsyncBoundary>
+      <InvitedLanding />;
+    </AsyncBoundary>
   );
 }
 
 export default Invited;
-
-Invited.getInitialProps = async function ({ req, query }: NextPageContext) {
-  const getSharedPackingListDetail = apiService.packingList.common.getSharedPackingListDetail;
-  const cookie = req?.headers?.cookie?.split(';');
-  const regex = new RegExp(/(?<=accessToken=).+/, 'g');
-  const accessToken = cookie && cookie[1].match(regex);
-
-  client.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-
-  const { data: info } = await getSharedPackingListDetail({
-    type: 'together',
-    inviteCode: query.inviteCode as string,
-  });
-  const { title } = info;
-  return {
-    title,
-  };
-};
