9e9de0644cbbeab7733a624f520442af637c3482
leeseooo <leeseoooo@naver.com>
2023-04-02 12:49:49 +0900

    feat/PK-27: 로그인 버튼과 이용약관 하단 텍스트 분리

--- utils/constant/index.ts
+++ utils/constant/index.ts
@@ -2,6 +2,9 @@ export const KAKAO_HREF = `https://kauth.kakao.com/oauth/authorize?client_id=${
   process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID
 }&redirect_uri=${encodeURI(process.env.NEXT_PUBLIC_REDIRECT ?? '')}&response_type=code`;
 
+export const PRIVACY_POLICY = 'https://www.notion.so/c9e91c3d19554ef7a01737d3fba6e102';
+export const TEMRS_OF_SERVICE = 'https://www.notion.so/99197c3491fe477ea9d69ed131cf4087';
+
 export const googleTagManagerId = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || '';
 
 declare global {
