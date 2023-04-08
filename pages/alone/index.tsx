20cd399daa83c04afc3e5df1266deedddb306aa7
n0eyes <doll4586633@naver.com>
2023-04-09 04:30:01 +0900

    refactor/PM-70: 불필요한 SSR 제거

--- pages/alone/index.tsx
+++ pages/alone/index.tsx
@@ -1,46 +1,12 @@
-import { NextPageContext } from 'next';
-import cookies from 'next-cookies';
-import { useRouter } from 'next/router';
 import AloneLanding from '../../components/alone/AloneLanding';
-import HeadMeta from '../../components/HeadMeta';
-import apiService from '../../service';
 import { AsyncBoundary } from '../../utils/AsyncBoundary';
-import { client } from '../../utils/axios';
 
-interface AloneProps {
-  title: string;
-}
-function Alone(props: AloneProps) {
-  const { title: headerTitle } = props;
-  const router = useRouter();
-
+function Alone() {
   return (
-    <>
-      <HeadMeta
-        title={headerTitle}
-        description={`[${headerTitle}] 패킹리스트가 공유되었어요!`}
-        url={`${process.env.NEXT_PUBLIC_DOMAIN}/${router.asPath}`}
-      />
-      <AsyncBoundary>
-        <AloneLanding />
-      </AsyncBoundary>
-    </>
+    <AsyncBoundary>
+      <AloneLanding />
+    </AsyncBoundary>
   );
 }
 
 export default Alone;
-
-export const getServerSideProps = async ({ req, query }: NextPageContext) => {
-  const getPackingListHeader = apiService.packingList.together.getPackingListHeader;
-
-  const allCookies = cookies({ req: { headers: { cookie: req?.headers.cookie } } });
-  const accessToken = allCookies['accessToken'];
-
-  Object.assign(client.defaults.headers, { Authorization: `Bearer ${accessToken}` });
-
-  const { data: header } = await getPackingListHeader(query.id as string, true);
-  const { title } = header;
-  return {
-    props: { title },
-  };
-};
--- pages/together/index.tsx
+++ pages/together/index.tsx
@@ -1,48 +1,12 @@
-import { useRouter } from 'next/router';
-import HeadMeta from '../../components/HeadMeta';
 import TogetherLanding from '../../components/together/TogetherLanding';
 import { AsyncBoundary } from '../../utils/AsyncBoundary';
-import { NextPageContext } from 'next';
-import apiService from '../../service';
-import { client } from '../../utils/axios';
-import cookies from 'next-cookies';
 
-interface TogetherProps {
-  title: string;
-}
-function Together(props: TogetherProps) {
-  const { title: headerTitle } = props;
-  const router = useRouter();
-
+function Together() {
   return (
-    <>
-      <HeadMeta
-        title={headerTitle}
-        description={`[${headerTitle}] 패킹리스트가 공유되었어요!`}
-        url={`${process.env.NEXT_PUBLIC_DOMAIN}/${router.asPath}`}
-      />
-      <AsyncBoundary>
-        <TogetherLanding />
-      </AsyncBoundary>
-    </>
+    <AsyncBoundary>
+      <TogetherLanding />
+    </AsyncBoundary>
   );
 }
 
 export default Together;
-
-export const getServerSideProps = async ({ req, query }: NextPageContext) => {
-  const getPackingListHeader = apiService.packingList.together.getPackingListHeader;
-
-  const allCookies = cookies({ req: { headers: { cookie: req?.headers.cookie } } });
-  const accessToken = allCookies['accessToken'];
-
-  Object.assign(client.defaults.headers, { Authorization: `Bearer ${accessToken}` });
-
-  const { data: header } = await getPackingListHeader(query.id as string, false);
-  const { title } = header;
-  return {
-    props: {
-      title,
-    },
-  };
-};
